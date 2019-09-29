952f21ee77cd50e2fe637f259bbdbf753b7d8623
DRINGOT <donovan.ringot@hotmail.fr>
2019-09-29 22:19:33 +0200

    declare variables

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const h1 = document.querySelector("h1");
+const scorePlayer1 = document.querySelectorAll("span")[0];
+const scorePlayer2 = document.querySelectorAll("span")[1];
+const winnerScore = document.querySelectorAll("span")[2];
+const input = document.querySelector("input");
+const player1Button = document.querySelectorAll("button")[0];
+const player2Button = document.querySelectorAll("button")[1];
+const reset = document.querySelectorAll("button")[2];
+
+let scoreP1 = 0;
+let scoreP2 = 0;
+let scoreMax = 5;
+
